cbf79628cc256d5dcf4537c94d27a3328327f45c
wabowen89 <wabowen89@gmail.com>
2023-06-12 14:55:30 -0700

    added reading and creating functionality to App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,77 @@ import Home from "./pages/Home";
 import mockCountries from "./mockCountries";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
 
-  const [countries, setCountries] = useState(mockCountries);
-  
-  const createCountry = (createdCountry) => {
-      console.log("my created country:", createdCountry);
-  }
-  const updateCountry = (country,id) => {
+  // const createCountry = (createdCountry) => {
+  //   console.log("my created country:", createdCountry);
+  // };
+
+  const updateCountry = (country, id) => {
     console.log(country);
-    console.log(id)
+    console.log(id);
+  };
+
+  useEffect(() => {
+    readCountry();
+  }, [])
+
+  const readCountry = () => {
+    fetch("http://localhost:3000/countries")
+      .then((response) => response.json())
+      .then((payload) => {
+        setCountries(payload);
+      })
+      .catch((error) => console.log(error));
   }
 
-  
-  
+  const createCountry = (createdCountry) => {
+    fetch("http://localhost:3000/countries", {
+      // converts the object to a string that can be passed in the request
+      body: JSON.stringify(createdCountry),
+      // specify the info being sent in JSON and the info returning should be JSON
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // HTTP verb so the correct endpoint is invoked on the server
+      method: "POST",
+    })
+      .then((response) => response.json())
+      .then((payload) => readCountry())
+      .catch((errors) => console.log("Country create errors:", errors));
+  }
+
+
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/countryedit/:id" element={<CountryEdit countries={countries} updateCountry={updateCountry} />} />
-        <Route path="/countryindex" element={<CountryIndex countries={countries} />} />
-        <Route path="/countrynew" element={<CountryNew createCountry={createCountry} />} />
-        <Route path="/countryshow/:id" element={<CountryShow countries={countries} />} />
+        <Route
+          path="/countryedit/:id"
+          element={
+            <CountryEdit countries={countries} updateCountry={updateCountry} />
+          }
+        />
+        <Route
+          path="/countryindex"
+          element={<CountryIndex countries={countries} />}
+        />
+        <Route
+          path="/countrynew"
+          element={<CountryNew createCountry={createCountry} />}
+        />
+        <Route
+          path="/countryshow/:id"
+          element={<CountryShow countries={countries} />}
+        />
         <Route path="/*" element={<NotFound />} />
-      </Routes> 
+      </Routes>
       <Footer />
     </>
   );
